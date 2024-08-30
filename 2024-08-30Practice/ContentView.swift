4777e28a57edbe4f8bbec7e13e2ff0887306285c
TKJapan <pianodoit@gmail.com>
2024-08-30 12:43:59 +0900

    Initial Commit

---
+++ 2024-08-30Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2024-08-30Practice
+//
+//  Created by Mac on 2024/08/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2024-08-30Practice/_024_08_30PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _024_08_30PracticeApp.swift
+//  2024-08-30Practice
+//
+//  Created by Mac on 2024/08/30.
+//
+
+import SwiftUI
+
+@main
+struct _024_08_30PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
